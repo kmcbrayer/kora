f0c794a44259d8b0d8224f0bfb8853a674ff6fb5
Kevin <kmcbrayer77@gmail.com>
2017-01-22 17:04:09 -0500

    fix routes

--- src/index.jsx
+++ src/index.jsx
@@ -1,16 +1,17 @@
 
 import React from 'react';
 import { render } from 'react-dom';
-import { Router, Route, Link, hashHistory } from 'react-router';
+import { Router, Route, IndexRoute, Link, browserHistory } from 'react-router';
 
 import App from './components/App.jsx';
 import NoMatch from './components/NoMatch.jsx';
+import LoginForm from './components/LoginForm.jsx';
 
 render((
-    <Router history={hashHistory}>
-        <Route path="/" component={App}>
-            <Route path="stuff" component={NoMatch}/>
-        </Route>
+    <Router history={browserHistory}>
+        <Route path="/" component={App}/>
+        <Route path="login" component={LoginForm}/>
+        <Route path="*" component={NoMatch}/>
     </Router>
     ),
     document.getElementById("content")
