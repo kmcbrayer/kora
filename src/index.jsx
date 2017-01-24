70d15fd324f011517556adfb49ea6fce78f332f8
Kevin <kmcbrayer77@gmail.com>
2017-01-24 03:27:28 -0500

    rename App to Home

--- src/index.jsx
+++ src/index.jsx
@@ -3,13 +3,13 @@ import React from 'react';
 import { render } from 'react-dom';
 import { Router, Route, IndexRoute, Link, browserHistory } from 'react-router';
 
-import App from './components/App.jsx';
+import Home from './components/Home.jsx';
 import NoMatch from './components/NoMatch.jsx';
 import LoginForm from './components/LoginForm.jsx';
 
 render((
     <Router history={browserHistory}>
-        <Route path="/" component={App}/>
+        <Route path="/" component={Home}/>
         <Route path="login" component={LoginForm}/>
         <Route path="*" component={NoMatch}/>
     </Router>
